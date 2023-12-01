cf9dac8340c382d9ecfa0dd470b6fd810f9adb37
miguel <fdezguerrerom@gmail.com>
2023-12-01 13:50:24 +0100

    1:50 pm commit

--- app/src/main/java/com/example/gestudent/SignInActivity.java
+++ app/src/main/java/com/example/gestudent/SignInActivity.java
@@ -3,9 +3,12 @@ package com.example.gestudent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignInActivity extends AppCompatActivity {
-
+    private EditText etName, etMail, etPassword, etConfPassword;
+    private Button btnSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
