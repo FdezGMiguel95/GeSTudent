2dd24733e64964ea77798772bc82d1d92f46c3c5
David_PC <hydruro@gmail.com>
2023-12-07 22:58:16 +0100

    Terminado RemeberPassword.java y xml

--- app/src/main/java/com/example/gestudent/RememberPassword.java
+++ app/src/main/java/com/example/gestudent/RememberPassword.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -29,6 +30,7 @@ public class RememberPassword extends AppCompatActivity {
         editText_RememberEmail = (EditText) findViewById(R.id.editText_RememberEmail);
         button_RememberPassword = (Button) findViewById(R.id.button_RememberPassword);
 
+        /* Otra forma de hacerlo en el xml con android:onClick="resetPassword"
         button_RememberPassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,9 +41,34 @@ public class RememberPassword extends AppCompatActivity {
                         if(task.isSuccessful()){
                             Intent i = new Intent(RememberPassword.this, LoginActivity.class);
                             startActivity(i);
+                            finish();
+                        } else {
+                            Toast.makeText(RememberPassword.this,"Error al enviar el correo electrónico. Verifique la dirección de correo electrónico.", Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
+            }
+        });*/
+    }
+
+    public void resetPassword(View view) {
+        email = editText_RememberEmail.getText().toString().trim();
+        if (email.isEmpty()) {
+            Toast.makeText(this, "Escriba su correo electrónico", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        mAuth.sendPasswordResetEmail(email).addOnCompleteListener(this, new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(RememberPassword.this,"Se ha enviado un correo electrónico para restablecer la contraseña.", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(RememberPassword.this, LoginActivity.class);
+                    startActivity(i);
+                    // Cierra la actividad después de enviar el correo electrónico
+                    finish();
+                } else {
+                    Toast.makeText(RememberPassword.this,"Error al enviar el correo electrónico. Verifique la dirección de correo electrónico.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
